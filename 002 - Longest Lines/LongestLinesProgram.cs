97a42fb7ca54f9fb62a4b70e4cdab4138ef02db0
Matt Oswald <m@oswald.io>
2016-04-30 11:22:53 -0500

    solves #002 - Longest Lines

---
+++ 002 - Longest Lines/LongestLinesProgram.cs
@@ -0,0 +1,20 @@
+﻿namespace CodeEval.LongestLines
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+
+    static class LongestLinesProgram
+    {
+        static void Main(string[] args)
+        {
+            var file = File.ReadAllLines(args[0]);
+            var count = int.Parse(file[0]);
+
+            foreach (var line in file.Skip(1).OrderByDescending(line => line.Length).Take(count))
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
