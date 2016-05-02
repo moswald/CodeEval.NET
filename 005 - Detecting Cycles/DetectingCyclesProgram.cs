c0071ee91666a6e73158c9b32bae1ee2ad98526c
Matt Oswald <m@oswald.io>
2016-05-02 16:49:41 -0500

    solves #005

---
+++ 005 - Detecting Cycles/DetectingCyclesProgram.cs
@@ -0,0 +1,37 @@
+﻿namespace CodeEval.DetectingCycles
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    static class DetectingCyclesProgram
+    {
+        static void Main(string[] args)
+        {
+            foreach (var result in File.ReadLines(args[0])
+                .AsParallel()
+                .Where(line => line != null)
+                .Select(line => line.Split(' ').Select(int.Parse).ToArray())
+                .Select(DetectCycle))
+            {
+                Console.WriteLine(result);
+            }
+        }
+
+        static string DetectCycle(IList<int> values)
+        {
+            var first = values.Last();
+
+            var cycle = values.Reverse()
+                .Skip(1)
+                .TakeWhile(x => x != first)
+                .Reverse()
+                .Aggregate(
+                    string.Empty,
+                    (s, value) => $"{s}{value} ");
+
+            return $"{cycle}{first}";
+        }
+    }
+}
