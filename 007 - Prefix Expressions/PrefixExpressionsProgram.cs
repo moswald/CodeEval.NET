cda40c1e2f28a7b7201b08b42030423825523409
Matt Oswald <m@oswald.io>
2016-05-07 16:47:00 -0500

    solved #007 - prefix expressions

---
+++ 007 - Prefix Expressions/PrefixExpressionsProgram.cs
@@ -0,0 +1,68 @@
+﻿namespace CodeEval.PrefixExpressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    static class PrefixExpressionsProgram
+    {
+        static void Main(string[] args)
+        {
+            foreach (var result in File.ReadLines(args[0])
+                .Where(line => line != null)
+                .Select(line => line.Split(' '))
+                .Select(tokens => new PrefixCalculator(tokens)))
+            {
+                Console.WriteLine(result.Result);
+            }
+        }
+
+        class PrefixCalculator
+        {
+            int _index;
+
+            public PrefixCalculator(IList<string> tokens)
+            {
+                Result = (int)Evaluate(tokens);
+            }
+
+            public int Result { get; }
+
+            float Evaluate(IList<string> tokens)
+            {
+                switch (tokens[_index])
+                {
+                    case "*":
+                    {
+                        ++_index;
+                        var a = Evaluate(tokens);
+                        var b = Evaluate(tokens);
+                        return a * b;
+                    }
+
+                    case "/":
+                    {
+                        ++_index;
+                        var a = Evaluate(tokens);
+                        var b = Evaluate(tokens);
+                        return a / b;
+                    }
+
+                    case "+":
+                    {
+                        ++_index;
+                        var a = Evaluate(tokens);
+                        var b = Evaluate(tokens);
+                        return a + b;
+                    }
+
+                    default:
+                    {
+                        return int.Parse(tokens[_index++]);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 007 - Prefix Expressions/readme.md
@@ -0,0 +1,28 @@
+#Challenge description:
+
+You are given a prefix expression. Write a program which evaluates it.
+
+#Input sample:
+
+Your program should accept a file as its first argument. The file contains one prefix expression per line.
+
+For example:
+
+    * + 2 3 4
+
+Your program should read this file and insert it into any data structure you like. Traverse this data structure and evaluate the prefix expression. Each token is delimited by a whitespace. You may assume that sum ‘+’, multiplication ‘*’ and division ‘/’ are the only valid operators appearing in the test data.
+
+#Output sample:
+
+Print to stdout the output of the prefix expression, one per line.
+
+For example:
+
+    20
+
+#Constraints
+
+* The evaluation result will always be an integer ≥ 0.
+* The number of the test cases is ≤ 40.
+
+https://www.codeeval.com/open_challenges/7/
