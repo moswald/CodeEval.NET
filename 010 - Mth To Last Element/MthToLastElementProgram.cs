c34e9397e85a24dcd7dde3bf93cb6d985dfb7c72
Matt Oswald <m@oswald.io>
2016-05-09 22:06:54 -0500

    solves #010

---
+++ 010 - Mth To Last Element/MthToLastElementProgram.cs
@@ -0,0 +1,26 @@
+﻿namespace CodeEval.MthToLastElement
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+
+    static class MthToLastElementProgram
+    {
+        static void Main(string[] args)
+        {
+            foreach (var output in File.ReadLines(args[0])
+                .Where(line => line != null)
+                .Select(line => line.Split(' '))
+                .Select(
+                    split =>
+                    {
+                        var index = int.Parse(split.Last());
+                        return split.Reverse().Skip(index).Take(1);
+                    })
+                .Where(result => result.Any()))
+            {
+                Console.WriteLine(output.Single());
+            }
+        }
+    }
+}
