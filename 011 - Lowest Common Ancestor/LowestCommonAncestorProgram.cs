7de3999689dd75368920115196988c8b75f0c5c4
Matt Oswald <m@oswald.io>
2016-05-10 21:56:47 -0500

    solves #011 - harder to do without recursion and without pointers-to-pointers (like C++)

---
+++ 011 - Lowest Common Ancestor/LowestCommonAncestorProgram.cs
@@ -0,0 +1,103 @@
+﻿namespace CodeEval.LowestCommonAncestor
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    static class LowestCommonAncestorProgram
+    {
+        static void Main(string[] args)
+        {
+            var root = BuildStaticTree();
+
+            foreach (var result in File.ReadLines(args[0])
+                .Where(line => line != null)
+                .Select(line => line.Split(' ')
+                    .Select(int.Parse)
+                    .OrderBy(x => x)
+                    .ToArray())
+                .Select(values => FindLowestCommonAncestor(root, values).Value))
+            {
+                Console.WriteLine(result);
+            }
+        }
+
+        static INode FindLowestCommonAncestor(INode root, IList<int> values)
+        {
+            while (true)
+            {
+                if (root.Value < values[0])
+                {
+                    root = root.Right;
+                    continue;
+                }
+
+                if (root.Value > values[1])
+                {
+                    root = root.Left;
+                    continue;
+                }
+
+                return root;
+            }
+        }
+
+        static INode BuildStaticTree()
+        {
+            INode root = new Node(30);
+
+            foreach (var value in new[] { 8, 52, 3, 20, 10, 29 })
+            {
+                var cur = root;
+                while (true)
+                {
+                    if (cur.Value < value)
+                    {
+                        if (cur.Right != null)
+                        {
+                            cur = cur.Right;
+                            continue;
+                        }
+
+                        cur.Right = new Node(value);
+                    }
+                    else
+                    {
+                        if (cur.Left != null)
+                        {
+                            cur = cur.Left;
+                            continue;
+                        }
+
+                        cur.Left = new Node(value);
+                    }
+
+                    break;
+                }
+            }
+
+            return root;
+        }
+
+        interface INode
+        {
+            int Value { get; }
+            INode Left { get; set; }
+            INode Right { get; set; }
+        }
+
+        struct Node : INode
+        {
+            public Node(int value)
+                : this()
+            {
+                Value = value;
+            }
+
+            public int Value { get; }
+            public INode Left { get; set; }
+            public INode Right { get; set; }
+        }
+    }
+}
---
+++ 011 - Lowest Common Ancestor/readme.md
@@ -0,0 +1,33 @@
+#Challenge description:
+
+Write a program to determine the lowest common ancestor of two nodes in a binary search tree. You may hardcode the following binary search tree in your program:
+
+        30
+        |
+      ____
+      |   |
+      8   52
+      |
+    ____
+    |   |
+    3  20
+        |
+       ____
+      |   |
+      10 29
+
+#Input sample:
+
+The first argument is a path to a file that contains two values. These values represent two nodes within the tree, one per line. E.g.:
+
+    8 52
+    3 29
+
+#Output sample:
+
+Print to stdout the lowest common ancestor, one per line. Lowest means the lowest depth in the tree, not the lowest value. E.g.:
+
+    30
+    8
+
+https://www.codeeval.com/open_challenges/11/
